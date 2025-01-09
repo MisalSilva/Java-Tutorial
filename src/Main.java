c5c2a7a82795c98f29ae37203930e4394b204f15
MisalSilva <misal.20230183@iit.ac.lk>
2025-01-09 20:01:50 +0530

    Add comments

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,14 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
+        // Variable declaration and assigning values
         String name = "Name";
         char character = 'A';
         int num = 10;
         double number = 3.14;
         boolean bool = true;
 
+        // Printing values
         System.out.println(name);
         System.out.println(character);
         System.out.println(num);
