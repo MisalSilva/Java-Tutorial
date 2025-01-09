a62754522dc85357c43571270ec3ffebcdda7621
MisalSilva <misal.20230183@iit.ac.lk>
2025-01-09 20:02:06 +0530

    variable declaration

---
+++ src/Tut02.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Tut02 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // Usual variable declaration
+        String name;
+        int age;
+        char character;
+        boolean bool;
+        double num;
+
+
+
+
+    }
+}
