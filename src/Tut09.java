d02fd40f7528724020e0ff3d32435996dd26261c
MisalSilva <misal.20230183@iit.ac.lk>
2025-01-11 14:20:57 +0530

    Add java do while loop and print answers

---
+++ src/Tut09.java
@@ -0,0 +1,27 @@
+// In this class we discuss java Do While loop
+public class Tut09 {
+    public static void main(String[] args) {
+
+        int i = 1;
+
+        do {
+            System.out.println("JAVA");
+            i++;
+        }
+        while(i<=10);
+
+//        The Difference between while loop and do while loop is:
+//        while loop checks the condition and only does the task if the condition is true
+//        But the do while loop will always will do the task once and then checks if the condition is true
+
+        int a = 17;
+
+        do {
+            System.out.println("JAVA");
+            a++;
+        }
+        while(a<=10);
+
+
+    }
+}
