4adb006d7fe55e011700c2c81b7b778b32bdfae3
MisalSilva <misal.20230183@iit.ac.lk>
2025-01-11 12:57:16 +0530

    Add java if else and print answers

---
+++ src/Tut05.java
@@ -0,0 +1,26 @@
+// In this class we discuss java If Else
+public class Tut05 {
+    public static void main(String[] args) {
+
+        int x = 10;
+
+        if (x == 11){
+            System.out.println("x is equal to 11");
+        }
+        else if (x == 12){
+            System.out.println("x is equal to 12");
+        }
+        else if (x != 10){
+            System.out.println("x is not equal to 10");
+        }
+        else if (x > 10){
+            System.out.println("x is greater than 10");
+        }
+        else if (x < 10){
+            System.out.println("x is less than 10");
+        }
+        else{
+            System.out.println("x is 10");
+        }
+    }
+}
