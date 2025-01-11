2b3a250cb2a224fb75f34ff3306254e0a3ba9c17
MisalSilva <misal.20230183@iit.ac.lk>
2025-01-11 12:19:40 +0530

    Add new operators and print answers

---
+++ src/Tut03.java
@@ -0,0 +1,20 @@
+// In this class we discuss Mathematical Operators
+public class Tut03 {
+    public static void main(String[]args){
+        int a = 10;
+        int b = 20;
+        int c,d,addition,subtraction,multiplication,division;
+        c = 12;
+        d = 5;
+        addition = c + d;
+        subtraction = c - d;
+        multiplication = c * d;
+        division = c / d;
+
+        System.out.println("Answer is "+addition);
+        System.out.println("Answer is "+subtraction);
+        System.out.println("Answer is "+multiplication);
+        System.out.println("Answer is "+division);
+
+    }
+}
