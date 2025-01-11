75498151514abb1795f9b953a357e70776ddb331
MisalSilva <misal.20230183@iit.ac.lk>
2025-01-11 14:08:51 +0530

    Add java while loop and print answers

---
+++ src/Tut08.java
@@ -0,0 +1,19 @@
+// In this class we discuss java While loop
+public class Tut08 {
+    public static void main(String[] args) {
+
+        int i = 1;
+        while(i<=5){
+            System.out.println("Hello");
+            i++;
+        }
+
+        int x = 10;
+        while(x<=20){
+            System.out.println(x);
+            x= x+2;
+        }
+
+
+    }
+}
