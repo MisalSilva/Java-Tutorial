599984b838784f964de5e7fe8effb8fd3f8e1517
MisalSilva <misal.20230183@iit.ac.lk>
2025-01-12 01:19:56 +0530

    Add java String and print answers

---
+++ src/Tut13.java
@@ -0,0 +1,29 @@
+// In this class we discuss java String
+public class Tut13 {
+    public static void main(String[]args){
+
+        // Two types of String declaration
+
+        String str1 = "Hello";
+        String str2 = "Hello";
+
+        if (str1==str2){
+            System.out.println("str1 and str2 are the same");
+        }
+
+        String str3 = new String("World");
+        String str4 = new String("World");
+
+        if (str3==str4){
+            System.out.println("str3 and str4 are the same");
+        }
+        else{
+            System.out.println("str3 and str4 are not the same");
+        }
+
+        // The difference between these to types is that,
+        // the first type assigns the variables to the same value and they both are the same since there's only one memory space.
+        // the second type assigns variables to two different values creating two memory spaces and they are not the same.
+
+    }
+}
