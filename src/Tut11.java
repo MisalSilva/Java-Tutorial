f0768e207d89a497d4c91e2adff255824f5659f0
MisalSilva <misal.20230183@iit.ac.lk>
2025-01-11 15:05:23 +0530

    Add java for loop and print answers

---
+++ src/Tut11.java
@@ -0,0 +1,18 @@
+// In this class we discuss java for loop
+public class Tut11 {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <=10; i++){
+            System.out.println("hello "+i);
+        }
+        for (int j=10; j>0;j--){
+            System.out.println("yoo "+j);
+        }
+
+        String[] names = {"gota","namal","maina"};
+        for (int i = 0; i < names.length; i++){
+            System.out.println(names[i]);
+        }
+
+    }
+}
