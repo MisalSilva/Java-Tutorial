fc7f9c57df23e096aff83c864931336baf80e427
MisalSilva <misal.20230183@iit.ac.lk>
2025-01-12 19:07:36 +0530

    Add java access modifiers in classes and print answers

--- src/Tut20/Nimal.java
+++ src/Tut20/Nimal.java
@@ -2,7 +2,7 @@ package Tut20;
 // In this class we discuss Java Access Modifiers in classes
 // public classes can be accessed through objects by other classes
 // Even if the classes are in different packages
-// default classes cannot be accessed by any means
+// Default classes are only accessible within the package
 public class Nimal {
     Saman obj2 = new Saman();
 }
