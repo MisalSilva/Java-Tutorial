71bfc5e58b42a8ae4fc44f6750e4e261f81126b8
MisalSilva <misal.20230183@iit.ac.lk>
2025-01-13 01:43:36 +0530

    Update Java Final Keyword

--- src/Tut27_Final_Keyword/Test.java
+++ src/Tut27_Final_Keyword/Test.java
@@ -1,4 +1,4 @@
-package Tut27;
+package Tut27_Final_Keyword;
 // In this class we discuss java final keyword
 /*final*/public class Test { // if stated final won't be able to inherit from this class
     /*final*/public void say(){ // if stated final won't be able to access from the child class
