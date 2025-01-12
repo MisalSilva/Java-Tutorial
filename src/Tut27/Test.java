3f798d6c086932b88d4a424223128b426a4ffe84
MisalSilva <misal.20230183@iit.ac.lk>
2025-01-12 23:34:05 +0530

    Add java final keyword and print answers

---
+++ src/Tut27/Test.java
@@ -0,0 +1,13 @@
+package Tut27;
+// In this class we discuss java final keyword
+/*final*/public class Test { // if stated final won't be able to inherit from this class
+    /*final*/public void say(){ // if stated final won't be able to access from the child class
+        System.out.println("Hello World");
+    }
+
+    public static void main(String[] args) {
+        final double g = 9.81;
+//        g=10; // can do changes to g if not stated as final
+        System.out.println(g);
+    }
+}
---
+++ src/Tut27/Test1.java
@@ -0,0 +1,12 @@
+package Tut27;
+
+public class Test1 extends Test{
+    public void say(){
+        System.out.println("Hellow");
+    } // won't be able to override if the method is named final the parent class
+
+    public static void main(String[] args) {
+        Test1 test = new Test1();
+        test.say();
+    }
+}
