3b00da571687431c7998c44480c35f22f7930b9e
MisalSilva <misal.20230183@iit.ac.lk>
2025-01-12 20:00:34 +0530

    Add java Method Overloading in Polymorphism and print answers

--- src/Tut22/Meth_Overloading.java
+++ src/Tut22/Meth_Overloading.java
@@ -1,5 +1,6 @@
 package Tut22;
 // In this class we discuss Method Overloading in polymorphism
+// The number of variables passed or variable's data types should be different
 public class Meth_Overloading {
 
     public void meth(){
