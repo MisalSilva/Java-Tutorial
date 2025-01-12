45feddf40e34e05337ba53665e8a712110f4dadc
MisalSilva <misal.20230183@iit.ac.lk>
2025-01-13 02:20:34 +0530

    Add Java Multiple Inheritance

---
+++ src/Tut29_Multiple_Inheritance_Interfaces/A.java
@@ -0,0 +1,7 @@
+package Tut29_Multiple_Inheritance_Interfaces;
+// Java Multiple Inheritance include,
+// class extends class and implements interface - extend for class and implements for interface
+// class implements two interfaces - implements for both interfaces
+// interface extends interface - extends for all interfaces
+public class A extends C implements B{
+}
