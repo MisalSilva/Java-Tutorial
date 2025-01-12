b5695f6f3780a11fb7a87b9328f69acb4cc9d24b
MisalSilva <misal.20230183@iit.ac.lk>
2025-01-12 16:31:57 +0530

    Add java constructor overloading and print answers

---
+++ src/Tut18/Bank.java
@@ -0,0 +1,24 @@
+package Tut18;
+// In this class we discuss constructor overloading
+public class Bank {
+    int rupees;
+
+    public Bank(){
+        rupees = 0;
+        System.out.println("Nimal Bank");
+    }
+    public Bank(int x){
+        rupees = x;
+        System.out.println("Kamal Bank");
+    }
+
+    public static void main(String[]args){
+        Bank nimal = new Bank(); // here nimal used the constructor with no parameters
+        System.out.println("Nimal's bank acct balance is "+nimal.rupees);
+
+        Bank kamal = new Bank(1000); // here kamal used the constructor with x as the parameter
+        System.out.println("kamal's bank acct balance is "+kamal.rupees);
+
+    }
+
+}
