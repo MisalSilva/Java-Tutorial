f0f641be6f28943cc060e1c45646898a428f2e2a
MisalSilva <misal.20230183@iit.ac.lk>
2025-01-20 19:52:45 +0530

    Add Java Multithreading with Runnable Interface and run Threads

---
+++ src/Tut34_Multithreading/Test3.java
@@ -0,0 +1,39 @@
+package Tut34_Multithreading;
+
+class C implements Runnable{
+    public void run(){
+        for(int i=0; i<10; i++){
+            System.out.println("Hello");
+            try{
+                Thread.sleep(500);
+            }catch(Exception e){}
+        }
+    }
+}
+class D implements Runnable{
+    public void run(){
+        for(int i=0; i<10; i++){
+            System.out.println("welcome");
+            try{
+                Thread.sleep(500);
+            }catch(Exception e){}
+        }
+    }
+}
+
+public class Test3 {
+    public static void main(String[]arg){
+        C c =  new C();
+        Thread t1 = new Thread(c);
+        t1.start();
+        try {
+            Thread.sleep(50);
+        }catch(Exception e){}
+        D d = new D();
+        Thread t2 = new Thread(d);
+        t2.start();
+        try{
+            Thread.sleep(50);
+        }catch(Exception e){}
+    }
+}
